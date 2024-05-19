8b474703518a5d80fa0cd0806657e3c33879e5d0
Boris Capitanu <capitanu@illinois.edu>
2024-05-18 22:14:47 -0500

    Added a kind of access logging

---
+++ app/filters/LoggingFilter.scala
@@ -0,0 +1,25 @@
+package filters
+
+import akka.stream.Materializer
+import play.api.Logging
+import play.api.mvc._
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class LoggingFilter @Inject() (implicit override val mat: Materializer, ec: ExecutionContext) extends Filter with Logging {
+  def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+    val startTime = System.currentTimeMillis
+
+    nextFilter(requestHeader).map { result =>
+      val endTime     = System.currentTimeMillis
+      val requestTime = endTime - startTime
+
+      logger.info(
+        s"${requestHeader.method} ${requestHeader.uri} took ${requestTime}ms and returned ${result.header.status}"
+      )
+
+      result
+    }
+  }
+}
