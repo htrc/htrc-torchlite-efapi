1e60a110556e2ac37506f735f8c956aea7d7bb32
Deren Kudeki <dkudeki@illinois.edu>
2026-06-03 07:28:00 -0500

    Try another method to show the contents

--- app/repo/EfRepositoryMongoImpl.scala
+++ app/repo/EfRepositoryMongoImpl.scala
@@ -69,7 +69,7 @@ class EfRepositoryMongoImpl @Inject()(val reactiveMongoApi: ReactiveMongoApi)
         import framework._
 
         val query = if (ids.isEmpty) document() else document("htid" -> document("$in" -> ids))
-        logger.debug(s"Sending query: ${query.elements}")
+        logger.debug(s"Sending query: ${BSONDocument.pretty(query)}")
 
         List(
           Match(query),
