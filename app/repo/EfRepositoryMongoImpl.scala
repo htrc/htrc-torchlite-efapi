f4734dcf9a2fe8bb6623c4eb199fc7afd30fef5c
Deren Kudeki <dkudeki@illinois.edu>
2026-06-03 06:33:54 -0500

    Try using BSONDocument function

--- app/repo/EfRepositoryMongoImpl.scala
+++ app/repo/EfRepositoryMongoImpl.scala
@@ -69,7 +69,7 @@ class EfRepositoryMongoImpl @Inject()(val reactiveMongoApi: ReactiveMongoApi)
         import framework._
 
         val query = if (ids.isEmpty) document() else document("htid" -> document("$in" -> ids))
-        logger.debug(s"Sending query: ${play.api.libs.json.Json.stringify(play.api.libs.json.Json.toJson(query))}")
+        logger.debug(s"Sending query: ${query.toString()}")
 
         List(
           Match(query),
