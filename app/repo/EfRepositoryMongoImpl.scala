895334c7ceacc6de099cb6caead0df4fec1caf18
Deren Kudeki <dkudeki@illinois.edu>
2026-06-02 23:27:17 -0500

    Fix log

--- app/repo/EfRepositoryMongoImpl.scala
+++ app/repo/EfRepositoryMongoImpl.scala
@@ -59,7 +59,7 @@ class EfRepositoryMongoImpl @Inject()(val reactiveMongoApi: ReactiveMongoApi)
   // ])
   //
   override def getVolumesAggNoPos(ids: IdSet, fields: List[String] = List.empty): Future[List[JsObject]] = {
-    logger.debug(ids)
+    logger.debug(s"getVolumeAggNoPos ids: ${ids}")
     val projFields = BSONDocument(fields.map(f => f -> BSONInteger(1)))
 
     for {
